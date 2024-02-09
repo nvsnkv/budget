39d0ccecc45d4d5d4071ad42cdc5f9c2460cf98b
Nikita V Senkov <nvsnkv@live.com>
2024-02-09 22:03:59 +0300

    chore: fixed nullability of substituted value in NamedParameterBuilder

--- src/Application/NVs.Budget.Domain.Tests/NamedParameterBuilder.cs
+++ src/Application/NVs.Budget.Domain.Tests/NamedParameterBuilder.cs
@@ -6,17 +6,17 @@ namespace NVs.Budget.Domain.Tests;
 internal class NamedParameterBuilder<T> : ISpecimenBuilder
 {
     private readonly string _name;
-    public readonly T _value;
+    private readonly T? _value;
     private readonly bool _ignoreCase;
 
-    public NamedParameterBuilder(string name, T value, bool ignoreCase)
+    public NamedParameterBuilder(string name, T? value, bool ignoreCase)
     {
         _name = name;
         _value = value;
         _ignoreCase = ignoreCase;
     }
 
-    public object Create(object request, ISpecimenContext context)
+    public object? Create(object request, ISpecimenContext context)
     {
         if (request is ParameterInfo pi && pi.ParameterType == typeof(T) && _name.Equals(pi.Name, _ignoreCase ? StringComparison.InvariantCultureIgnoreCase : StringComparison.InvariantCulture))
         {
