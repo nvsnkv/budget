cc1222bcc0a80204e9a4677532f210d39cb71f6b
Nikita Senkov <nvsnkv@live.com>
2024-04-21 00:44:30 +0300

    feat: error handling in superverb handler

--- src/Controllers/NVs.Budget.Controllers.Console.Handlers/Commands/SuperVerbHandler.cs
+++ src/Controllers/NVs.Budget.Controllers.Console.Handlers/Commands/SuperVerbHandler.cs
@@ -1,13 +1,16 @@
 ﻿using CommandLine;
+using CommandLine.Text;
 using MediatR;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using NVs.Budget.Controllers.Console.Contracts.Commands;
 using NVs.Budget.Controllers.Console.Contracts.IO.Options;
+using NVs.Budget.Controllers.Console.Contracts.IO.Output;
 using NVs.Budget.Controllers.Console.IO.Output;
 
 namespace NVs.Budget.Controllers.Console.Handlers.Commands;
 
-internal class SuperVerbHandler<T>(IMediator mediator, Parser parser, IOutputOptionsChanger outputOptionsChanger) : IRequestHandler<T, ExitCode> where T : SuperVerb
+internal class SuperVerbHandler<T>(IMediator mediator, Parser parser, IOutputOptionsChanger outputOptionsChanger, IOutputStreamProvider streams, IOptionsSnapshot<OutputOptions> options) : IRequestHandler<T, ExitCode> where T : SuperVerb
 {
     public async Task<ExitCode> Handle(T request, CancellationToken cancellationToken)
     {
@@ -22,7 +25,19 @@ internal class SuperVerbHandler<T>(IMediator mediator, Parser parser, IOutputOpt
         }
 
         var parserResult = parser.ParseArguments(request.Args, request.Verbs);
-        var result = await parserResult.MapResult(o => mediator.Send(o, cancellationToken), errs => Task.FromResult((object?)-1));
+        var result = await parserResult.MapResult(o => mediator.Send(o, cancellationToken), async errs =>
+        {
+            var helpText = HelpText.AutoBuild(parserResult);
+            var errors = errs as Error[] ?? errs.ToArray();
+            var isHelp = errors.IsHelp() || errors.IsVersion();
+            var writer = isHelp
+                ? await streams.GetOutput(options.Value.OutputStreamName)
+                : await streams.GetError(options.Value.ErrorStreamName);
+
+            await writer.WriteLineAsync(helpText);
+            await writer.FlushAsync(cancellationToken);
+            return (object?)(isHelp ? ExitCode.Success : ExitCode.ArgumentsError);
+        });
 
         if (result is ExitCode value)
         {
