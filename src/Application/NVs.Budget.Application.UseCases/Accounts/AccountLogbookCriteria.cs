5a6b19aa8cbac4439de34d2d392efec49043edf6
Nikita Senkov <nvsnkv@live.com>
2024-07-07 12:41:02 +0300

    chore(accounts): fixed visibility & updated filenames

--- src/Application/NVs.Budget.Application.UseCases/Accounts/AccountLogbookCriteria.cs
+++ src/Application/NVs.Budget.Application.UseCases/Accounts/AccountLogbookCriteria.cs
@@ -4,7 +4,7 @@ using NVs.Budget.Domain.ValueObjects.Criteria;
 
 namespace NVs.Budget.Application.UseCases.Accounts;
 
-public class AccountLogbookCriterion(params TrackedAccount[] accounts) : Criterion("Accounts", accounts.Select(CreateSubcriterion))
+internal class AccountLogbookCriterion(params TrackedAccount[] accounts) : Criterion("Accounts", accounts.Select(CreateSubcriterion))
 {
     private static Criterion CreateSubcriterion(TrackedAccount account) => new PredicateBasedCriterion($"{account.Bank}: {account.Name}", o => o.Account.Id == account.Id);
 
