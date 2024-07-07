0db2b1052093e83fadbb164981f09e6288b0cb15
Nikita Senkov <nvsnkv@live.com>
2024-07-07 15:40:59 +0300

    fix(accounts): registered merge as valid command

--- src/Controllers/NVs.Budget.Controllers.Console.Handlers/Commands/Accounts/AccountsVerb.cs
+++ src/Controllers/NVs.Budget.Controllers.Console.Handlers/Commands/Accounts/AccountsVerb.cs
@@ -6,4 +6,4 @@ using NVs.Budget.Controllers.Console.Contracts.Commands;
 namespace NVs.Budget.Controllers.Console.Handlers.Commands.Accounts;
 
 [Verb("acc", HelpText = "Accounts handling")]
-internal class AccountsVerb() : SuperVerb([typeof(AccountStatisticsVerb)]);
+internal class AccountsVerb() : SuperVerb([typeof(AccountStatisticsVerb),typeof(MergeAccountsVerb)]);
