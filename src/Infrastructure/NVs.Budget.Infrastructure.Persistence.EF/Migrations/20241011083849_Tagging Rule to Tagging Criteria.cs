375ed78920a5570bee7f680a44c83c8bcd3698c0
Nikita Senkov <nvsnkv@live.com>
2024-10-11 11:41:22 +0300

    fix: added missing dbMigration

---
+++ src/Infrastructure/NVs.Budget.Infrastructure.Persistence.EF/Migrations/20241011083849_Tagging Rule to Tagging Criteria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NVs.Budget.Infrastructure.Persistence.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaggingRuletoTaggingCriteria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "StoredTaggingRule",
+                newName: "StoredTaggingCriterion",
+                schema: "budget");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "StoredTaggingCriterion",
+                newName: "StoredTaggingRule",
+                schema: "budget");
+        }
+    }
+}
