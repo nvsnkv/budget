f1742c43dfc361b678046c5585239e7347eda1a4
Nikita Senkov <nvsnkv@live.com>
2024-10-31 08:51:24 +0300

    feat: best match for transfer criteria

--- src/Application/NVs.Budget.Application/Services/Accounting/Transfers/TransferDetector.cs
+++ src/Application/NVs.Budget.Application/Services/Accounting/Transfers/TransferDetector.cs
@@ -16,20 +16,26 @@ internal class TransferDetector(IReadOnlyList<TransferCriterion> criteria)
         if (sink.Amount.Amount <= 0) return Result.Fail(new SinkIsNotAnIncomeError(sink));
         if (!source.Amount.HasSameCurrencyAs(sink.Amount)) return Result.Fail(new SourceAndSinkHaveDifferentCurrenciesError(source, sink));
 
+        TransferCriterion? bestMatch = null;
         foreach (var criterion in criteria)
         {
             if (criterion.Criterion.AsInvokable()(source, sink))
             {
-                return new TrackedTransfer(source, sink, criterion.Comment)
+                if (bestMatch is null || bestMatch.Accuracy < criterion.Accuracy)
                 {
-                    Accuracy = criterion.Accuracy
-                };
+                    bestMatch = criterion;
+                }
             }
         }
 
-        return Result.Fail(new NoTransferCriteriaMatchedError()
-            .WithMetadata(nameof(source), source.Id)
-            .WithMetadata(nameof(sink), sink.Id));
+        return bestMatch is not null
+            ? new TrackedTransfer(source, sink, bestMatch.Comment)
+            {
+                Accuracy = bestMatch.Accuracy
+            }
+            : Result.Fail(new NoTransferCriteriaMatchedError()
+                .WithMetadata(nameof(source), source.Id)
+                .WithMetadata(nameof(sink), sink.Id));
     }
 }
 
