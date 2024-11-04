aed0561f7136dde958f44200cdfebef10a51848a
Nikita Senkov <nvsnkv@live.com>
2024-11-04 13:31:49 +0300

    chore: improved error logs

--- src/Infrastructure/NVs.Budget.Infrastructure.IO.Console.Tests/CsvFileParserShould.cs
+++ src/Infrastructure/NVs.Budget.Infrastructure.IO.Console.Tests/CsvFileParserShould.cs
@@ -26,7 +26,7 @@ public class CsvFileParserShould(TestBed testBed) : IClassFixture<TestBed>
 
         var name = "validFile.csv";
         var operations = await parser.ReadUnregisteredOperations(new StreamReader(stream), options.GetFileOptionsFor(name).Value, CancellationToken.None).ToListAsync();
-        operations.Should().AllSatisfy(r => r.Should().BeSuccess());
+        operations.Should().AllSatisfy(r => r.Should().BeSuccess($"{r.PrintoutReasons()}"));
         operations.Select(o => o.Value).Should().BeEquivalentTo(ValidFile.Operations);
     }
 
