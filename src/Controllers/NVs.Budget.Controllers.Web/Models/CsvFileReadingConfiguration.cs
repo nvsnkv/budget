1643eb7bf6a914a2c1e2b5d419add687edde47f7
Nikita V Senkov <nvsnkv@live.com>
2025-06-22 17:45:50 +0300

    fix: added default values for not-null fields

--- src/Controllers/NVs.Budget.Controllers.Web/Models/CsvFileReadingConfiguration.cs
+++ src/Controllers/NVs.Budget.Controllers.Web/Models/CsvFileReadingConfiguration.cs
@@ -1,15 +1,15 @@
 namespace NVs.Budget.Controllers.Web.Models;
 
 public class CsvFileReadingConfiguration : Dictionary<string, string> {
-    public string CultureCode { get; init; }
+    public string CultureCode { get; init; } = string.Empty;
     public DateTimeKind DateTimeKind { get; init; }
     public IReadOnlyDictionary<string, string>? Attributes {get; init;}
     public IReadOnlyDictionary<string, string>? ValidationRules {get; init;}
 
     public class ValidationRule {
-        public string FieldConfiguration {get; init;}
-        public string Value {get; init;}
-        public ValidationCondition Condition {get; init;}
+        public string FieldConfiguration {get; init;} = string.Empty;
+        public string Value {get; init;} = string.Empty;
+        public ValidationCondition Condition {get; init;} = ValidationCondition.Equals;
     }
 
     public enum ValidationCondition {
