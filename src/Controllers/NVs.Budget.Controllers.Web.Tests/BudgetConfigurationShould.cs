f2aaa8ebdad29fe65ccca4f5c201e554a40d33be
Nikita Senkov <nvsnkv@live.com>
2025-06-23 22:30:07 +0300

    tests: budgetConfig serialization

---
+++ src/Controllers/NVs.Budget.Controllers.Web.Tests/BudgetConfigurationShould.cs
@@ -0,0 +1,43 @@
+using AutoFixture;
+using AutoMapper;
+using FluentAssertions;
+using NVs.Budget.Application.Contracts.Criteria;
+using NVs.Budget.Application.Contracts.Entities.Budgeting;
+using NVs.Budget.Controllers.Web.Models;
+using NVs.Budget.Utilities.Expressions;
+using NVs.Budget.Utilities.Testing;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace NVs.Budget.Controllers.Web.Tests;
+
+public class BudgetConfigurationShould
+{
+    private readonly Fixture _fixture = new();
+    private readonly Mapper _mapper = new(new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile(ReadableExpressionsParser.Default))));
+    private readonly IDeserializer _yamlDeserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+    private readonly ISerializer _yamlSerializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+
+    public BudgetConfigurationShould()
+    {
+        _fixture.Customizations.Add(new ReadableExpressionsBuilder());
+        _fixture.Inject(LogbookCriteria.Universal);
+    }
+
+    [Fact]
+    public void SerializeAndDeserializeBudgetConfiguration()
+    {
+        var budget = _fixture.Create<TrackedBudget>();
+        budget.SetTransferCriteria(new[] { new TransferCriterion(
+            DetectionAccuracy.Exact, 
+            "Test", 
+            ReadableExpressionsParser.Default.ParseBinaryPredicate<TrackedOperation, TrackedOperation>("(l, r) => true").Value
+            )});
+
+        var configuration = _mapper.Map<BudgetConfiguration>(budget);
+        var yaml = _yamlSerializer.Serialize(configuration);
+        var deserialized = _yamlDeserializer.Deserialize<BudgetConfiguration>(yaml);
+
+        deserialized.Should().BeEquivalentTo(configuration);
+    }
+}
