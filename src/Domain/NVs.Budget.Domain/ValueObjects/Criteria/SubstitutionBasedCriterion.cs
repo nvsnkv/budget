73e8fc1c5a560b39cbe809967c8bf195092ee589
Nikita Senkov <nvsnkv@live.com>
2025-11-30 22:20:08 +0300

    chore(logbook): order substitutions alphabetically

--- src/Domain/NVs.Budget.Domain/ValueObjects/Criteria/SubstitutionBasedCriterion.cs
+++ src/Domain/NVs.Budget.Domain/ValueObjects/Criteria/SubstitutionBasedCriterion.cs
@@ -2,35 +2,29 @@ using NVs.Budget.Domain.Entities.Operations;
 
 namespace NVs.Budget.Domain.ValueObjects.Criteria;
 
-public class SubstitutionBasedCriterion : Criterion
+public class SubstitutionBasedCriterion(string description, Func<Operation, string> substitution) : Criterion(description)
 {
     private static readonly UniversalCriterion Dummy = new(string.Empty);
 
-    private readonly List<PredicateBasedCriterion> _subcriteria = [Dummy];
-    private readonly Func<Operation, string> _substitution;
+    private readonly SortedList<string, PredicateBasedCriterion> _subcriteria = new() { { Dummy.Description, Dummy } };
 
-    public SubstitutionBasedCriterion(string description, Func<Operation, string> substitution) : base(description)
-    {
-        _substitution = substitution;
-    }
-
-    public override IReadOnlyList<Criterion> Subcriteria => _subcriteria.AsReadOnly();
+    public override IReadOnlyList<Criterion> Subcriteria => _subcriteria.Values.AsReadOnly();
 
     public override bool Matched(Operation t) => true;
 
-    public override Criterion? GetMatchedSubcriterion(Operation t)
+    public override Criterion GetMatchedSubcriterion(Operation t)
     {
         var found = base.GetMatchedSubcriterion(t);
         if (found is not null && !ReferenceEquals(found, Dummy)) return found;
 
-        if (_subcriteria.All(c => ReferenceEquals(c, Dummy)))
+        if (_subcriteria.All(c => ReferenceEquals(c.Value, Dummy)))
         {
             _subcriteria.Clear();
         }
 
-        var description = _substitution(t);
-        var criterion = new PredicateBasedCriterion(description, o => _substitution(o) == description);
-        _subcriteria.Add(criterion);
+        var description = substitution(t);
+        var criterion = new PredicateBasedCriterion(description, o => substitution(o) == description);
+        _subcriteria.Add(description, criterion);
         return criterion;
     }
 }
