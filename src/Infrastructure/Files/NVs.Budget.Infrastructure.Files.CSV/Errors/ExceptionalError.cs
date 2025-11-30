58ed5565fef63316388d544b5cc555a8365373ee
Nikita Senkov <nvsnkv@live.com>
2025-11-30 21:08:31 +0300

    fix(operations):  fixed import parsing errors serialization

--- src/Infrastructure/Files/NVs.Budget.Infrastructure.Files.CSV/Errors/ExceptionalError.cs
+++ src/Infrastructure/Files/NVs.Budget.Infrastructure.Files.CSV/Errors/ExceptionalError.cs
@@ -5,7 +5,6 @@ namespace NVs.Budget.Infrastructure.Files.CSV.Errors;
 internal class ConversionError(Exception exception) : IError
 {
     public string Message => exception.Message;
-    public Dictionary<string, object> Metadata { get; } = new() { { nameof(exception), exception } };
+    public Dictionary<string, object> Metadata { get; } = new() { { nameof(exception), exception.ToString() } };
     public List<IError> Reasons { get; } = new();
 }
-
